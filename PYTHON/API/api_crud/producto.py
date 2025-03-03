696827e66e8b21a319158d042d4a1bb64a348dc8
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2025-03-03 22:05:11 +0100

    Título añadido

--- PYTHON/API/api_crud/producto.py
+++ PYTHON/API/api_crud/producto.py
@@ -10,16 +10,16 @@ class Producto(BaseModel):
     is_offer: Union[bool, None] = None
 
 
-@app.get("/")
+@app.get("/productos")
 def get_all_productos():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/producto/{item_id}")
 def get_producto(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.put("/items/{item_id}")
+@app.put("/producto/{item_id}")
 def update_producto(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
